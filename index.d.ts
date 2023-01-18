851756808fe691c5c1f05fe4deebd1190d1bf220
christopher-estanis <christopher.estanis@gmail.com>
2023-01-17 22:29:20 -0300

    impr: add isOptionalField value into check function in args

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,32 @@
 interface IRule {
-    validator: Function;
-    message: string;
+  validator: Function;
+  message: string;
+  optional?: boolean;
 }
 interface IFieldsAndRules {
-    field: string;
-    rules: IRule[];
+  field: string;
+  rules: IRule[];
 }
 interface ICheck {
-    field: string;
-    message: string;
+  field: string;
+  message: string;
 }
+
+interface ICheckObj {
+  [key: string]: any
+  isOptionalField?: boolean
+}
+
 declare class RequestCheck {
-    rules: any;
-    requiredMessage: string;
-    useFieldNameAsKey: boolean;
-    constructor();
-    setRequiredMessage: (message: string) => void;
-    addRule: (field: string, ...rules: IRule[]) => void;
-    addRules: (field: string, rules: IRule[]) => void;
-    addFieldsAndRules: (fieldsAndRules: IFieldsAndRules[]) => void;
-    check: (...args: Array<any>) => Array<ICheck> | any | undefined;
+  rules: any;
+  requiredMessage: string;
+  useFieldNameAsKey: boolean;
+  constructor();
+  setRequiredMessage: (message: string) => void;
+  addRule: (field: string, ...rules: IRule[]) => void;
+  addRules: (field: string, rules: IRule[]) => void;
+  addFieldsAndRules: (fieldsAndRules: IFieldsAndRules[]) => void;
+  check: (...args: Array<ICheckObj>) => Array<ICheck> | any | undefined;
 }
 declare const requestCheck: () => RequestCheck;
 export default requestCheck;
